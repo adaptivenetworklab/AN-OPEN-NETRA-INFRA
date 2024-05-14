4ef41516b07532e6267f2f426830d49bc4896110
arigints <gintingari73@gmail.com>
2024-05-14 19:49:36 +0700

    add folder.groovy

---
+++ folder.groovy
@@ -0,0 +1,14 @@
+folder('infra') {
+    description('this folder contains all infrastructure related jobs')
+}
+folder('website') {
+    description('this folder contains all website related jobs')
+}
+
+folder('website/production') {
+    description('website production jobs')
+}
+
+folder('website/staging') {
+    description('website staging jobs')
+}
