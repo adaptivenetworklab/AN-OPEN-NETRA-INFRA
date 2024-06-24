c337724288078b1e74c255eb60af6e1015844287
arigints <gintingari73@gmail.com>
2024-06-24 10:34:18 +0700

    add casc and its dependencies

---
+++ jcasc/create-folder.groovy
@@ -0,0 +1,35 @@
+folder('infrastructure') {
+    description('this folder contains all infrastructure related jobs')
+}
+
+folder('infra/argocd') {
+    description('this folder contains all nginx related jobs')
+}
+
+folder('infra/jenkins') {
+    description('this folder contains all prometheus related jobs')
+}
+
+folder('infra/nginx') {
+    description('this folder contains all DNS adaptivenetlab.site related jobs')
+}
+
+folder('infra/longhorn') {
+    description('this folder contains all DNS adaptivenetworklab.org related jobs')
+}
+
+folder('orca') {
+    description('this folder contains all website related jobs')
+}
+
+folder('orca/production') {
+    description('website production jobs')
+}
+
+folder('orca/staging') {
+    description('website staging jobs')
+}
+
+folder('5gcore') {
+    description('this folder contains all gaboot related jobs')
+}
